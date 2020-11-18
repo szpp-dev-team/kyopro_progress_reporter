8a151a657a0220f65cbb25b2702f248d1ea310f8
earlgray <earlgray283@gmail.com>
2020-11-19 00:38:19 +0900

    さわりだけ

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+)
+
+func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal(err)
+	}
+
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+
+	http.HandleFunc("/slack/events", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		switch eventsAPIEvent.Type {
+		case slackevents.URLVerification:
+			var res *slackevents.ChallengeResponse
+			if err := json.Unmarshal(body, &res); err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "text/plain")
+			if _, err := w.Write([]byte(res.Challenge)); err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+		case slackevents.CallbackEvent:
+			innerEvent := eventsAPIEvent.InnerEvent
+			switch event := innerEvent.Data.(type) {
+			case *slackevents.AppMentionEvent:
+				message := strings.Split(event.Text, " ")
+				if len(message) < 2 {
+					if _, _, err := api.PostMessage(event.Channel, slack.MsgOptionText("What's up?", false)); err != nil {
+						log.Println(err)
+						w.WriteHeader(http.StatusInternalServerError)
+					}
+					return
+				}
+
+				command := message[1]
+				switch command {
+				case "get":
+					if _, _, err := api.PostMessage(event.Channel, slack.MsgOptionText("get up!", false)); err != nil {
+						log.Println(err)
+						w.WriteHeader(http.StatusInternalServerError)
+						return
+					}
+				}
+			}
+		}
+	})
+
+	log.Println("[INFO] Server listening...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
